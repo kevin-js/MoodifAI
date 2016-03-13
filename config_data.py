78883b60c9d2bba356807ac0f37f2dde7fb0a36b
Kevin Guh <kevinjguh@yahoo.com>
2016-03-12 23:24:30 -0500

    config_data added for heroku app

---
+++ config_data.py
@@ -0,0 +1,15 @@
+"""
+	filename: config_data.py
+	description: file containing configuration data for the Ambience app
+	date: 2/13/2016
+"""
+
+facebook_app_token = "1706170612960650"
+facebook_secret_key = "ed2f904c049c274ebecb08b5e06503e1"
+twitter_consumer_key = 'Qh6UnD63kwXXFYRKQSFz6mMhb'
+twitter_consumer_secret = 'DXq5ECjTTm40cnx041k7LNDxhHd3CuHyiCLRP1dhDoAYS9Xjke'
+# personal access key; change for production site
+twitter_access_key = '3399788060-DW1ok0vDzFzVEmHJcE5fwuGbXMdoqsbaLkpddXQ'
+twitter_access_secret = 'Rg5DPBAzPQQsXMi6ZWobLA5DvdDmasEPXXECOeuejxhH2'
+
+echonest_api_key = '7IRRDM32L59SUG0MC'
