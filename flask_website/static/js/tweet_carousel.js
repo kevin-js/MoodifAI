8023cbcfafc008ee15f9b653ea756d836f372c74
Kevin Guh <kevinjguh@yahoo.com>
2016-03-13 20:20:27 -0400

    hallelujah it works

---
+++ flask_website/static/js/tweet_carousel.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function(){
+	var counter = 0;
+	window.setInterval(function(){
+		$('#carousel').html(
+			myTweets[counter % myTweets.length]
+		);
+		counter+=;
+	}, 3000);
+});*/
